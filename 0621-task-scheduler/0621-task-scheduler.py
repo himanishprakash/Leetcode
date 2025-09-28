0f128e9a4b93b8c818a3e87ebf14bf65ddc5bccb
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-28 13:35:51 -0700

    Time: 16 ms (89.04%), Space: 19 MB (90.73%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,26 +1,23 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
 
-        element = [0] * 26
+        counts = [0] * 26
 
         for task in tasks:
-
-            element[ord(task) - ord('A')] += 1
-
-        element.sort()
-
-        max_Slot = element[25] - 1
-
-        idle_slot = max_Slot * n
-
-        for i in range(24,-1,-1):
-            idle_slot -= min(max_Slot, element[i])
+            counts[ord(task) - ord('A')] += 1
 
         
-        if idle_slot > 0:
-            return idle_slot + len(tasks)
 
+        counts.sort()
+
+        max_slot = counts[25] - 1
+        idle_slots = max_slot * n
+
+        for i in range(24, -1, -1):
+            idle_slots -= min(max_slot, counts[i])
+
+        if idle_slots >0:
+            return idle_slots + len(tasks)
         else:
             return len(tasks)
-
         
