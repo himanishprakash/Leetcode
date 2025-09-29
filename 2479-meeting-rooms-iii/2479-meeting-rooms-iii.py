fd21deeeb31ff9b587bf21fce2f5c085cb7fd1f6
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-29 16:59:44 -0700

    Time: 777 ms (7.85%), Space: 51.4 MB (69.51%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.py
@@ -5,26 +5,26 @@ class Solution:
         meeting_count = [0] * n
 
         for start, end in sorted(meetings):
-
-            min_available_time_room = inf
+            min_available_time = inf
             min_avail_time = 0
-            find_unused_room =False
+            room_avail = False
 
             for i in range(n):
 
                 if room_avail_time[i] <= start:
-                    find_unused_room =True
+                    room_avail = True
                     room_avail_time[i] = end
                     meeting_count[i] += 1
                     break
 
-                if min_available_time_room > room_avail_time[i]:
-                    min_available_time_room = room_avail_time[i]
+                if min_available_time > room_avail_time[i]:
+                    min_available_time = room_avail_time[i]
                     min_avail_time = i
 
-            if not find_unused_room:
+                
+            if not room_avail:
                 room_avail_time[min_avail_time] += end -start
-                meeting_count[min_avail_time] += 1
+                meeting_count[min_avail_time] += 1 
 
         
         return meeting_count.index(max(meeting_count))
@@ -32,7 +32,3 @@ class Solution:
 
 
 
-
-
-
-        
