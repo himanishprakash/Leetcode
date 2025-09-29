f4eee7478a5cf3072f676df6c6ea31977a4b765d
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-29 13:27:00 -0700

    Time: 156 ms (47.45%), Space: 53.1 MB (95.29%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.py
@@ -3,28 +3,28 @@ class Solution:
 
         n = len(events)
         max_day = 0
-        for event in events:
-            max_day = max(max_day, event[1])
+
+        for i in events:
+            max_day = max(max_day, i[1])
 
         events.sort()
-        arr = []
+        array = []
+        answer = j = 0
 
-        ans = j =0
-
-        for i in range(1, max_day + 1):
+        for i in range(1,max_day + 1):
 
             while j < n and events[j][0] <= i:
-                heapq.heappush(arr, events[j][1])
-                j += 1
+                heapq.heappush(array,events[j][1] )
+                j +=1
+                
 
-            while arr and arr[0] < i:
-                heapq.heappop(arr)
+            while array and array[0] < i:
+                heapq.heappop(array)
 
-            if arr:
-                heapq.heappop(arr)
-                ans += 1
+            if array:
+                heapq.heappop(array)
+                answer += 1
 
-        return ans
+        return answer
 
-        
         
