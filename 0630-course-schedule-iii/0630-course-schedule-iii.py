69c8adcb27eeef216124c66db2b40a256c39579c
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-28 18:08:05 -0700

    Time: 57 ms (26.76%), Space: 21.7 MB (38.97%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -2,19 +2,21 @@ class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         
         courses.sort(key = lambda x: x[1])
-
+        
+        max_heap = []
         total_time = 0
 
-        max_heap = []
-
-        for duration, deadline in courses:
+        for duration, deadline in courses: 
             total_time += duration
 
             heapq.heappush(max_heap, -duration)
+
             if total_time > deadline:
-                
                 longest = -heapq.heappop(max_heap)
                 total_time -= longest
 
 
         return len(max_heap)
+
+
+
