7707515da1a5ccf5f207a77579f09f2cf022a7fa
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-30 07:41:00 -0700

    Time: 19 ms (85.74%), Space: 19.1 MB (65.51%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,6 +1,6 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-
+        
         task = [0] * 26
 
         for t in tasks:
@@ -9,13 +9,10 @@ class Solution:
         task.sort()
 
         max_slot = task[25] - 1
-
         idle_slot = max_slot * n
 
         for i in range(24, -1, -1):
-
-            idle_slot -= min(max_slot,task[i])
-
+            idle_slot -= min(max_slot,task[i] )
 
         if idle_slot > 0:
             return idle_slot + len(tasks)
