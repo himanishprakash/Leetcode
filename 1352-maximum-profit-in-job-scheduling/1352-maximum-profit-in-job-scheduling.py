a5dedc73d914e9be33eabbffd4de02725788d863
Himanish Prakash <himanishprakash23@gmail.com>
2025-09-29 17:20:40 -0700

    Time: 247 ms (33%), Space: 30.3 MB (77.19%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -3,32 +3,38 @@ class Solution:
 
         def binary_search(target):
             low, high = 0, n
-            
+
             while low < high:
                 mid = (low + high) // 2
+
                 if starts[mid] < target:
                     low = mid + 1
                 else:
                     high = mid
 
             return low
-        
 
-        jobs = sorted(zip(startTime,endTime, profit))
+        jobs = sorted(zip(startTime, endTime,profit ))
+        n = len(jobs)
         starts = []
+
         for s, _, _ in jobs:
             starts.append(s)
 
-        n = len(jobs)
-
         dp = [0] * (n + 1)
 
+        for i in range(n -1 , -1 ,-1):
 
-        for i in range(n -1, -1, -1):
-            s,e,p = jobs[i]
+            start , end, profit = jobs[i]
+
+            j = binary_search(end)
+           
+
+            dp[i] = max(dp[i + 1], profit + dp[j])
             
-            j = binary_search(e)
-            dp[i] = max(dp[i + 1], p + dp[j])
-            
-        
+
+
         return dp[0]
+
+        
+        
