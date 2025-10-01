cd0ae93c9048a6811602356557b3a8cb59282dff
Himanish Prakash <himanishprakash23@gmail.com>
2025-10-01 10:44:11 -0700

    Time: 105 ms (89.22%), Space: 77.9 MB (91.9%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,27 +1,29 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        
+
         self.capacity = capacity
         self.dict = OrderedDict()
-
-    def get(self, key: int) -> int:
         
+    def get(self, key: int) -> int:
         if key not in self.dict:
             return -1
         
         self.dict.move_to_end(key)
         return self.dict[key]
+
         
 
     def put(self, key: int, value: int) -> None:
+
         if key in self.dict:
             self.dict.move_to_end(key)
 
         self.dict[key] = value
 
         if len(self.dict) > self.capacity:
-            self.dict.popitem(False)        
+            self.dict.popitem(False)
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
