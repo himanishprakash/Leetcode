2d205928f3759992eae84db47885f7d557906524
Himanish Prakash <himanishprakash23@gmail.com>
2026-07-22 13:26:08 -0700

    Time: 1 ms (43.49%), Space: 21.6 MB (6.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,30 +4,31 @@ class Solution:
 
         if not intervals:
             return [newInterval]
-        n = len(intervals)
-        left , right = 0, n -1
 
+        n = len(intervals)
+        left , right = 0, n - 1
         target = newInterval[0]
 
-        while left <=right:
+        while left <= right:
+
             mid = (left + right) // 2
 
             if intervals[mid][0] < target:
                 left = mid + 1
-            else:
-                right = mid -1
 
+            else:
+                right = mid - 1
 
         intervals.insert(left,newInterval)
-
+        
         result = []
-
         for i in intervals:
 
             if not result or result[-1][1] < i[0]:
                 result.append(i)
             else:
                 result[-1][1] = max(result[-1][1], i[1])
+
 
 
         return result
