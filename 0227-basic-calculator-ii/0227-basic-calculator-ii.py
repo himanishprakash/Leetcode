684f20375dbb7c5641285553d5deb5a9c898896a
Himanish Prakash <himanishprakash23@gmail.com>
2026-07-22 17:47:03 -0700

    Time: 35 ms (97.29%), Space: 22.5 MB (24.66%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,31 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        
 
         s += '+'
-        num = 0
-        prev_op = '+'
+        num = 0 
+        prev_opr = '+'
         stack = []
 
         for i in s:
             if i.isdigit():
                 num = num * 10 + int(i)
-                
+
             elif i == ' ':
                 continue
             else:
-                if prev_op == '+':
+                if prev_opr == "+":
                     stack.append(num)
-                elif prev_op == '-':
+                elif prev_opr == "-":
                     stack.append(-num)
-                elif prev_op == '*':
+                elif prev_opr == "*":
                     prev_num = stack.pop()
-                    stack.append(num * prev_num)
-                elif prev_op == '/':
+                    stack.append(prev_num * num)
+                elif prev_opr == "/":
                     prev_num = stack.pop()
-                    stack.append(trunc(prev_num/ num))
-                
+                    stack.append(trunc(prev_num / num))
+
                 num = 0
-                prev_op = i
+                prev_opr = i
 
         return sum(stack)
-        
